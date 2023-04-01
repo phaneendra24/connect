50b7f7b4b132645b82af16826ea8e6f087d003ed
phaneendra <phaneendrapilli777@gmail.com>
2023-04-01 08:20:15 +0530

    setup commit

---
+++ components/navbar.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Navbar() {
+  return <div className="h-20 w-20 bg-white text-white">asdfasdfds</div>;
+}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/navbar";
+
+function App() {
+  return (
+    <div className="App bg-[#363636] min-h-screen">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
